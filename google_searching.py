a5ce850f67e0a122f68c63d55ffd06359bc798b8
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-09-23 00:13:30 +0300

    v0.3
    1. The maximum number of results increased (depending on keywords can be about 500)
    2. Global session requests to save cookies between different keyword searches

--- google_searching.py
+++ google_searching.py
@@ -1,41 +1,53 @@
-from lxml import html
+from time import sleep
+
 import requests
+from lxml import html
 
-__version__ = 0.2
+__version__ = 0.3
 
-def get_url(url):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
-    url = f"https://{url}" if ('https://' not in url) else url
-    resp = requests.get(url, headers=headers, timeout=5)
-    if resp.status_code == 200:
-        return resp
-    
-def ggl(query, max_results=20):
-    query  = query.replace(' ','+')
-    url = f'https://www.google.com/search?hl=en&q={query}&num={max_results}'
-    resp = get_url(url)
-    tree = html.fromstring(resp.text)
+session = requests.Session()
+session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"})
+
+def ggl(keywords, max_results=20):
+    query  = keywords.replace(' ','+')
     results = []
-    elements = tree.xpath("//div[@class='g']")
-    for g in elements:
-        snippets = g.xpath('.//div/div/div[2]/div')
-        snippet = None
-        rich_snippet = None
-        if len(snippets) == 1:
-            snippet = snippets[0].text_content()
-        elif len(snippets) > 1:
-            if len(snippets[1].xpath('.//g-review-stars')) > 0:
-                rich_snippet = snippets[1].text_content()
-                snippet = snippets[0].text_content()
-            else:
-                snippet = snippets[1].text_content()
-                rich_snippet = snippets[0].text_content()
+    num = min(max_results, 100)
+    start = 0
+    while True:
+        url = f'https://www.google.com/search?q={query}&num={max_results}&hl=en&&start={start}'
+        resp = session.get(url, timeout=5)
+        if resp.status_code == 429:
+            print('Сaptcha! Return None')
+            return None
+        if resp.status_code == 200:
+            tree = html.fromstring(resp.text)
+            temp_results = []
+            elements = tree.xpath("//div[@class='g']")
+            if not elements:
+                return results
+            for g in elements:
+                snippets = g.xpath('.//div/div/div[2]/div')
+                snippet = None
+                rich_snippet = None
+                if len(snippets) == 1:
+                    snippet = snippets[0].text_content()
+                elif len(snippets) > 1:
+                    if len(snippets[1].xpath('.//g-review-stars')) > 0:
+                        rich_snippet = snippets[1].text_content()
+                        snippet = snippets[0].text_content()
+                    else:
+                        snippet = snippets[1].text_content()
+                        rich_snippet = snippets[0].text_content()
+                res = {            
+                    'title': g.xpath('.//h3/text()')[0],
+                    'href': g.xpath('.//@href[1]')[0],
+                    'body': snippet,
+                    'rich_body': rich_snippet,
+                    }
+                temp_results.append(res)
 
-        res = {            
-            'title': g.xpath('.//h3/text()')[0],
-            'href': g.xpath('.//@href[1]')[0],
-            'body': snippet,
-            'rich_body': rich_snippet,
-        }
-        results.append(res)
-    return results
+            start += num
+            results.extend(temp_results)
+            if len(results) >= max_results or abs(max_results - len(results)) <= 10:
+                return results
+            sleep(5)
