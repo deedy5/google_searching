018340fdd92b5761d64f12aff6b38ade2a4658c3
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-11-10 10:07:16 +0300

    v0.5
    Update useragent

--- google_searching.py
+++ google_searching.py
@@ -3,10 +3,10 @@ from time import sleep
 import requests
 from lxml import html
 
-__version__ = '0.4'
+__version__ = '0.5'
 
 session = requests.Session()
-session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"})
+session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"})
 
 def ggl(keywords, max_results=20):
     query  = keywords.replace(' ','+')
