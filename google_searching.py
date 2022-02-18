1791e0d291f4cb9c7fab0ef7c19503538dd2e1d6
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-02-18 12:07:19 +0300

    v0.7

--- google_searching.py
+++ google_searching.py
@@ -3,51 +3,47 @@ from time import sleep
 import requests
 from lxml import html
 
-__version__ = '0.6'
+__version__ = '0.7'
 
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"})
 
-def ggl(keywords, max_results=20):
+
+def ggl(keywords, lang='en', max_results=20):
     query  = keywords.replace(' ','+')
     results = []
-    num = min(max_results + 5, 100)
     start = 0
+    num = min(max_results + 5, 100)
+    
     while True:
-        url = f'https://www.google.com/search?q={query}&num={num}&hl=en&start={start}'
+        url = f'https://www.google.com/search?q={query}&num={num}&hl={lang}&start={start}'
         resp = session.get(url, timeout=5)
+        
         if resp.status_code == 429:
             print('Сaptcha! Return None')
             return None
+        
         if resp.status_code == 200:
             tree = html.fromstring(resp.text)
-            temp_results = []
-            elements = tree.xpath("//div[@class='g']")
+            elements = tree.xpath("//div[contains(@class,'g ')]")
+            
             if not elements:
                 return results
-            for g in elements:
-                snippets = g.xpath('.//div/div/div[2]/div')
-                snippet = None
-                rich_snippet = None
-                if len(snippets) == 1:
-                    snippet = snippets[0].text_content()
-                elif len(snippets) > 1:
-                    if len(snippets[1].xpath('.//g-review-stars')) > 0:
-                        rich_snippet = snippets[1].text_content()
-                        snippet = snippets[0].text_content()
-                    else:
-                        snippet = snippets[1].text_content()
-                        rich_snippet = snippets[0].text_content()
-                res = {            
-                    'title': g.xpath('.//h3//text()')[0],
-                    'href': g.xpath('.//@href[1]')[0],
-                    'body': snippet,
-                    'rich_body': rich_snippet,
-                    }
-                temp_results.append(res)
-
-            start += num
-            results.extend(temp_results)
+            
+            for e in elements:
+                title = e.xpath('.//h3//text()')
+                if title:
+                    href = e.xpath('.//@href[1]')
+                    body = e.xpath("./div/div/div[2]/div[1] | ./div/div[2]/div[1]")
+                    res = {            
+                        'title': title[0],
+                        'href': href[0],
+                        'body': body[0].text_content() if body else "",
+                        }
+                results.append(res)
+            
             if len(results) >= max_results or abs(max_results - len(results)) <= 10:
                 return results
-            sleep(5)
+
+            start += num            
+        sleep(2)
