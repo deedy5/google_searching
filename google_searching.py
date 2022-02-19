f8e7c3e31e1e3634d2d59e90ea302766bdd6630b
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-02-19 07:22:24 +0300

    v0.8

--- google_searching.py
+++ google_searching.py
@@ -1,9 +1,10 @@
+import unicodedata
 from time import sleep
 
 import requests
 from lxml import html
 
-__version__ = '0.7'
+__version__ = '0.8'
 
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"})
@@ -13,11 +14,11 @@ def ggl(keywords, lang='en', max_results=20):
     query  = keywords.replace(' ','+')
     results = []
     start = 0
-    num = min(max_results + 5, 100)
-    
+    num = min(max_results + 5, 100)    
+
     while True:
-        url = f'https://www.google.com/search?q={query}&num={num}&hl={lang}&start={start}'
-        resp = session.get(url, timeout=5)
+        url = f'https://www.google.com/search?q={query}&num={num}&hl={lang}&start={start}&gbv=1'
+        resp = session.get(url, timeout=10)
         
         if resp.status_code == 429:
             print('Сaptcha! Return None')
@@ -25,7 +26,7 @@ def ggl(keywords, lang='en', max_results=20):
         
         if resp.status_code == 200:
             tree = html.fromstring(resp.text)
-            elements = tree.xpath("//div[contains(@class,'g ')]")
+            elements = tree.xpath("//div[./div/a/h3]")
             
             if not elements:
                 return results
@@ -33,17 +34,17 @@ def ggl(keywords, lang='en', max_results=20):
             for e in elements:
                 title = e.xpath('.//h3//text()')
                 if title:
-                    href = e.xpath('.//@href[1]')
-                    body = e.xpath("./div/div/div[2]/div[1] | ./div/div[2]/div[1]")
+                    href = e.xpath('.//a/@href[1]')
+                    body = e.xpath("./div[2]")
                     res = {            
                         'title': title[0],
-                        'href': href[0],
-                        'body': body[0].text_content() if body else "",
+                        'href': href[0].lstrip('/url?q=').split('&')[0],
+                        'body': unicodedata.normalize("NFKD", body[0].text_content()) if body else "",
                         }
                 results.append(res)
             
             if len(results) >= max_results or abs(max_results - len(results)) <= 10:
                 return results
-
-            start += num            
-        sleep(2)
+            
+            start += num
+        sleep(5)
