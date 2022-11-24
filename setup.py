8330f7130b3d9c7f9b3a6803c7e3166e882789a5
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-11-24 14:40:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
         'Topic :: Software Development :: Libraries :: Python Modules',
